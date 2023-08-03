3ad183e0ba9fa52e8a4003b30ad00c7e2f2c8f2a
Kartikaya Dwivedi <kartikaya2000@gmail.com>
2023-08-03 16:08:34 +0530

    Time: 87 ms (44.97%), Space: 27.7 MB (68.61%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& board) 
+    {
+        //Approach :- Take Edge O's as our Source and apply BFS or DFS on the O's and else will become X.
+        int n=board.size();
+        int m =board[0].size();
+        
+        queue<pair<int,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j =0;j<m;j++)
+            {
+                if(board[i][j]==1 && (i ==0 || i==n-1 || j==0 || j==m-1))
+                {
+                    q.push({i,j});
+                    vis[i][j]=1;
+                    cout<<i<<j;
+                }
+            }
+        }
+        vector<int>delrow={-1,0,1,0};
+	    vector<int>delcol={0,-1,0,1};
+
+        while(!q.empty())
+        {
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i =0;i<4;i++)
+	        {
+	            int row=r+delrow[i];
+	            int col=c+delcol[i];
+	            if(row>=0 && row<n && col>=0 && col<m && board[row][col]==1 && vis[row][col]==0)
+	            {
+                    vis[row][col]=1;
+	                q.push({row,col});
+                    cout<<row<<col;
+	            }
+	        }
+        }
+        int cnt=0;
+        for(int i =0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(vis[i][j]==0 && board[i][j]==1)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
