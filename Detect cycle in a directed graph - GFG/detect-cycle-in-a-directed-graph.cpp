8e6bf4d68a6e7638236264d919b3b86aa53bf742
Kartikaya Dwivedi <kartikaya2000@gmail.com>
2023-08-05 00:26:21 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,34 +6,36 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfs(vector<int>adj[], int i , vector<int>&vis,vector<int>&pathvis)
-    {
-        vis[i]=1;
-        pathvis[i]=1;
-        for(int num:adj[i])
-        {
-            if(!vis[num])
-            {
-                if (dfs(adj,num,vis,pathvis)==true)return true;
-            }
-            else if (pathvis[num]==1)
-            return true;
-        }
-        pathvis[i]=0;
-        return false;
-    }
     bool isCyclic(int n, vector<int> adj[]) 
     {
-        vector<int>vis(n,0);
-        vector<int>pathvis(n,0);
-        for (int  i=0;i<n;i++)
+        queue<int>q;
+        vector<int>indegree(n,0);
+        for(int i =0;i<n;i++)
         {
-            if(!vis[i])
+            for (int num:adj[i])
+                indegree[num]++;
+        }
+        //Multisource BFS
+        for (int i =0;i<n;i++)
+        {
+            if (indegree[i]==0)
             {
-                if (dfs(adj,i,vis,pathvis)==true) return true;
+                q.push(i);
             }
         }
-        return false;
+        
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            for (auto num:adj[node])
+            {
+                indegree[num]--;
+                if (indegree[num]==0)
+                    q.push(num);
+            }
+        }
+        return accumulate(indegree.begin(),indegree.end(),0)?true:false;
     }
 };
 
