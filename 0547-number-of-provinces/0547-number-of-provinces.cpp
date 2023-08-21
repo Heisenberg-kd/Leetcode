b772ecc6e4684a6f435a0990da3b781e48de14f9
Kartikaya Dwivedi <kartikaya2000@gmail.com>
2023-08-22 01:35:23 +0530

    Time: 24 ms (76.15%), Space: 15.4 MB (72.16%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,48 +1,77 @@
-class Solution {
+class DisjointSet {
+    vector<int> rank, parent, size;
 public:
-    void dfs(int node, vector<int> adj[], int vis[])
-    {
-        vis[node]=1;
-        for(auto i : adj[node])
-        {
-            if(!vis[i])
-            {
-                dfs(i,adj,vis);
-            }
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) 
     {
+        //using DSU;
         int n=isConnected.size();
-        int m=isConnected[0].size();
-        vector<int>adj[n];
-        for(int i=0;i<n;i++)
+        DisjointSet ds(n+1);
+        for(int i =0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for (int j=0;j<n;j++)
             {
-                if(isConnected[i][j]==1 && i!=j)
+                if (isConnected[i][j]==1)
                 {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                    ds.unionBySize(i+1,j+1);
                 }
             }
         }
-        //Applying DFS
-        int vis[n];
-        for(int i=0;i<n;i++)
+        int sum=0;
+        for (int i=0;i<n;i++)
         {
-            vis[i]=0;
+            if(ds.findUPar(i+1)==i+1)
+            sum++;
         }
-        int count=0;
-        for(int i =0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                count++;
-                dfs(i,adj,vis);
-            }
-        }
-        return count;
+        return sum;
+
         
     }
 };
